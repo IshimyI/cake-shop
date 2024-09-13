d1f2b0e244f62fcd8b7cb2c1a317ac9c9da21e0a
Ivan Borisenko <shimy92@mail.ru>
2024-09-13 21:28:33 +0300

    form completed

---
+++ script.js
@@ -0,0 +1,9 @@
+// https://webhook.site/942c6750-f6e9-4136-a0ed-f50de1f4da7e
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  alert(JSON.stringify(data));
+});
