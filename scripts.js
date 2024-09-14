5635f2317751f6028c861ade222686eab993a199
Ivan Borisenko <shimy92@mail.ru>
2024-09-14 21:51:58 +0300

    questions bug fixed

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// https://webhook.site/942c6750-f6e9-4136-a0ed-f50de1f4da7e
-
-const form = document.querySelector("#form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-});
